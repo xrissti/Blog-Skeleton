02c37fbe163181f5cf03301bfed1028c54e85912
Hristina Petkova <xrissti@gmail.com>
2017-05-01 23:56:24 +0300

    Add tests to blog

---
+++ Blog.UI.Tests/BrowseHost.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.Seleno;
+using TestStack.Seleno.Configuration;
+
+namespace Blog.UI.Tests
+{
+    public static class BrowseHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl = @"http://localhost:60634/Article/List";
+
+        static BrowseHost()
+        {
+            Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+        }
+    }
+}
---
+++ Blog.UI.Tests/UITests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.Seleno.PageObjects.Actions;
+
+namespace Blog.UI.Tests
+{
+    [TestFixture]
+    public class UITests
+    {
+        [Test]
+        public void CheckSiteLoad()
+        {
+            IWebDriver driver = BrowseHost.Instance.Application.Browser;
+            WebDriverWait wait = new WebDriverWait(driver,TimeSpan.FromSeconds(60));
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("http://localhost:60634/Article/List");
+            var logo = wait.Until(w => w.FindElement(By.XPath("/html/body/div[1]/div/div[1]/a")));
+            Assert.AreEqual("SOFTUNI BLOG",logo.Text);
+
+            driver.Quit();
+        }
+    }
+}
